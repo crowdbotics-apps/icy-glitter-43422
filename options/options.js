6a2ddbfbb626ae64a08ba57b40e09e54f3cbc90c
Crowdbotics <team@crowdbotics.com>
2023-09-16 20:32:01 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,15 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "icy-glitter-43422",
+  url: "https://icy-glitter-43422.botics.co",
+  api: "https://icy-glitter-43422.botics.co/api/v1",
+  screenOptions: {
+    headerShown: false
+  }
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
